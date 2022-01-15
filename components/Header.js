bed77b7fbb7fb5c7c42b95df85268dfb71987712
JethroSama <kel73896@gmail.com>
2022-01-15 18:47:04 +0800

    Add other items

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import HeaderItem from './HeaderItem'
-import { HomeIcon } from '@heroicons/react/outline'
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from '@heroicons/react/outline'
 
 const Header = () => {
     return (
         <header >
-            <div className="pt-5">
+            <div className="pt-5 flex">
                 <HeaderItem title="Home" Icon={HomeIcon} />
-
+                <HeaderItem title="trending" Icon={LightningBoltIcon} />
+                <HeaderItem title="verified" Icon={BadgeCheckIcon} />
+                <HeaderItem title="collections" Icon={CollectionIcon} />
+                <HeaderItem title="search" Icon={SearchIcon} />
+                <HeaderItem title="account" Icon={UserIcon} />
             </div>
             <Image height={100} width={200} src="https://links.papareact.com/ua6" className="object-contain" alt="Hulu logo" />
         </header>
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderItem = ({ title, Icon }) => {
     return (
         <div className="group flex flex-col w-12 sm:w-20 items-center">
-            <Icon className="h-8 group-hover:animate-bounce" />
+            <Icon className="h-8 group-hover:animate-bounce mb-1" />
             <p className="tracking-widest uppercase hidden group-hover:block ">{title}</p>
         </div>
     )
