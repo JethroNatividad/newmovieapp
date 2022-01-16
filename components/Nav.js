0a40dc72923cbed7d613d833175858b1e70444fa
JethroSama <kel73896@gmail.com>
2022-01-16 18:37:43 +0800

    make nav working

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import navItems from '../utils/fetch'
+
 const Nav = () => {
+    const router = useRouter()
+    const [currentGenre, setCurrentGenre] = React.useState(navItems[0].id)
+
+    useEffect(() => {
+        // get genre from url
+        const genre = router.query.genre
+        setCurrentGenre(genre)
+
+    }, [])
+
+    const handleNavClick = (id) => {
+        router.push(`/?genre=${id}`)
+        setCurrentGenre(id)
+    }
+
     return (
         <nav className="relative">
             <div className="absolute top-0 left-0 w-[5%] h-full bg-gradient-to-r from-bluish" />
-            <div className="flex px-10 sm:px-20 space-x-10 sm:space-x-20 overflow-scroll h-auto hide-scroll text-2xl sm:text-3xl justify-center">
+            <div className="flex px-10 sm:px-20 space-x-10 sm:space-x-20 overflow-y-hidden overflow-x-scroll h-auto hide-scroll text-2xl sm:text-3xl">
 
-                {navItems.map(({ id, title, endpoint }) => (<p key={id} className="hover:text-orange-200 cursor-pointer transition-all whitespace-nowrap">{title}</p>))}
+                {navItems.map(({ id, title, endpoint }) => (<p key={id} onClick={() => handleNavClick(id)} className={`hover:text-orange-200 hover:scale-125 py-2 cursor-pointer transition-all whitespace-nowrap ${currentGenre === id && 'text-orange-300 scale-125'}`}>{title}</p>))}
 
             </div>
             <div className="absolute top-0 right-0 w-1/12 h-full bg-gradient-to-l from-bluish" />
