5009c0c3139ce054546bbd24df4be5aa3f1b6fd4
JethroSama <kel73896@gmail.com>
2022-01-16 18:21:12 +0800

    restyle nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,13 @@
 import React from 'react'
-
+import navItems from '../utils/fetch'
 const Nav = () => {
     return (
         <nav className="relative">
             <div className="absolute top-0 left-0 w-[5%] h-full bg-gradient-to-r from-bluish" />
-            <div className="flex px-10 sm:px-20 space-x-10 sm:space-x-20 overflow-scroll h-fit hide-scroll text-2xl sm:text-3xl justify-center ">
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
-                <p className="hover:text-4xl cursor-pointer transition-all">Hello</p>
+            <div className="flex px-10 sm:px-20 space-x-10 sm:space-x-20 overflow-scroll h-auto hide-scroll text-2xl sm:text-3xl justify-center">
+
+                {navItems.map(({ id, title, endpoint }) => (<p key={id} className="hover:text-orange-200 cursor-pointer transition-all whitespace-nowrap">{title}</p>))}
+
             </div>
             <div className="absolute top-0 right-0 w-1/12 h-full bg-gradient-to-l from-bluish" />
         </nav>
