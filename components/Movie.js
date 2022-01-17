5f003ca7aec7e7adf77692d9ae75b008c6b336ff
JethroSama <kel73896@gmail.com>
2022-01-18 00:42:30 +0800

    transfer movie to own component

--- components/Movie.js
+++ components/Movie.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Movie = () => {
+const Movie = ({ movie }) => {
     return (
-        <div>
-
+        <div >
+            <Image layout='responsive' src={`https://image.tmdb.org/t/p/original${movie.poster_path || movie.backdrop_path}`} height={1080} width={1920} alt={movie.title} />
+            <h1>{movie.title || movie.original_name}</h1>
         </div>
     )
 }
--- components/MoviesList.js
+++ components/MoviesList.js
@@ -1,14 +1,11 @@
-import Image from 'next/image'
 import React from 'react'
+import Movie from './Movie'
 
 const MoviesList = ({ movies }) => {
     return (
         <div>
             {movies.map(movie => (
-                <div key={movie.id}>
-                    <h1>{movie.title || movie.original_name}</h1>
-                    <Image layout='responsive' src={`https://image.tmdb.org/t/p/original${movie.poster_path || movie.backdrop_path}`} height={1080} width={1920} alt={movie.title} />
-                </div>
+                <Movie key={movie.id} movie={movie} />
             ))}
         </div>
     )
