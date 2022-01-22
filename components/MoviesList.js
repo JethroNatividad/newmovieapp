bb5e758eb3c40a2450f11545535057d56a0e2636
JethroSama <kel73896@gmail.com>
2022-01-22 14:48:11 +0800

    css grid

--- components/MoviesList.js
+++ components/MoviesList.js
@@ -3,7 +3,7 @@ import Movie from './Movie'
 
 const MoviesList = ({ movies }) => {
     return (
-        <div>
+        <div className='px-5 py-5 sm:grid sm:grid-cols-2 xl:grid-cols-3'>
             {movies.map(movie => (
                 <Movie key={movie.id} movie={movie} />
             ))}
