20d34b1e81b316d060c86e1c8d8bf2786ecc9ea5
JethroSama <kel73896@gmail.com>
2022-01-22 14:51:51 +0800

    bg fix

--- components/MoviesList.js
+++ components/MoviesList.js
@@ -3,7 +3,7 @@ import Movie from './Movie'
 
 const MoviesList = ({ movies }) => {
     return (
-        <div className='px-5 py-5 sm:grid sm:grid-cols-2 xl:grid-cols-3'>
+        <div className='px-5 py-5 sm:grid sm:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
             {movies.map(movie => (
                 <Movie key={movie.id} movie={movie} />
             ))}
